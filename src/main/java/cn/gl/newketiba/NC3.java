316ca45f203ad1fff1cf4c82f837ae138894b75b
GLBB <unbeatableglbb@gmail.com>
2021-04-13 23:23:00 +0800

    链表中环入口节点

---
+++ src/main/java/cn/gl/newketiba/NC3.java
@@ -0,0 +1,32 @@
+package cn.gl.newketiba;
+
+public class NC3 {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast == null) {
+                return null;
+            }
+            fast = fast.next;
+            if (fast == null) {
+                return null;
+            }
+            if (slow == fast) {
+                break;
+            }
+        }
+        fast = head;
+        while (fast != slow) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
